cb1eaeb37feca7c8beb1b76a106bda740eb01d79
takibul islam <takibulislam.ti@gmail.com>
2024-06-01 09:52:58 +0600

    2nd commit - code refactored

--- tests/google-search-results.spec.js
+++ tests/google-search-results.spec.js
@@ -1,23 +1,23 @@
-import { test, expect } from "@playwright/test";
-import { title } from "process";
+const { test, expect } = require("@playwright/test");
 
 test("Google search", async ({ page }) => {
   await page.goto("https://www.bing.com/");
 
+  // Search on the search engine
   await page.locator('input[id="sb_form_q"]').fill("javascript");
   await page.keyboard.press("Enter");
 
   await page.waitForSelector("#b_results");
 
+  // priting search results using js code inside playwright scripts.
   const searchResults = await page.evaluate(() => {
     let results = [];
+
     const searchItems = document.querySelectorAll("#b_results>.b_algo");
     searchItems.forEach((result) => {
       const titles = result.querySelector("h2").innerText;
-      // console.log(titles);
       results.push(titles);
     });
-    // results.push(titles);
     return results;
   });
   console.log(searchResults);
